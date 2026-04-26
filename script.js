b40586a194d14c2753c37bc0eecd9bd7b7c2a4d6
Milenna Alves <milennafeijocanal@gmail.com>
2026-04-26 20:39:03 -0300

    feat: adiciona variáveis, função, if else if e resultado do cálculo do IMC

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+const botao = document.getElementById ('botao')
 
-const botao = document.getElementById('botao');
-
-
-function imc() {
+function calcularIMC() {
     const nome = document.getElementById('nome').value;
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
     const resultado = document.getElementById('resultado');
 
     if (nome !== '' && peso !== '' && altura !== '') {
-        alert('valor preenchido com sucesso');
-       
+	const imc = (peso / (altura * altura)).toFixed(1);
+    const nomeFormatado = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
+    let classificacao = '';
+    let cor = '';
+
+    if (imc < 18.5) {
+        classificacao = 'Magreza';
+        cor = 'red';
+    } else if (imc >= 18.5 && imc < 25) {
+        classificacao = 'Peso normal';
+        cor = 'green';
+    } else if (imc >= 25 && imc < 29.9) { 
+        classificacao = 'Sobrepeso';
+        cor = 'orange';
+    } else if (imc >= 30 && imc < 34.9) {
+        classificacao = 'Obesidade grau 1';
+        cor = 'red';
+    } else if (imc >= 35 && imc < 39.9) {
+        classificacao = 'Obesidade grau 2';
+        cor = 'red';
     } else {
-        alert('preencha todos os campos');
+        classificacao = 'Obesidade grau 3';
+        cor = 'purple';
+    }
+
+    resultado.innerHTML = `Olá, ${nomeFormatado}, seu IMC é ${imc}kg/m².<br>Você está classificado(a) com ${classificacao}!`;   
+    resultado.style.display = 'block';
+    resultado.style.color = cor;
+
+    } else {
+        resultado.textContent = 'Por favor, preencha todos os campos.';
+        resultado.style.display = 'block';
+        resultado.style.color = 'red';
     }
 }
 
-botao.addEventListener('click', imc);
+botao.addEventListener('click', calcularIMC);
